e6041db81876c8fa4e6fca02870c0cde17dd33a9
Márton Borsos <borsos.marton95@gmail.com>
2022-03-13 15:30:20 +0100

    Using web3 to convert to wei

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -1,6 +1,6 @@
 from scripts.helpful_scripts import get_account
 from brownie import interface, config, network, accounts
-import sys
+from web3 import Web3
 
 
 def main():
@@ -13,6 +13,7 @@ def get_weth():
     """
     account = get_account()
     weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
-    tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
+    tx = weth.deposit({"from": account, "value": Web3.toWei(0.1, "ether")})
     tx.wait(1)
     print("Received 0.1 WETH")
+    return tx
